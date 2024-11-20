4fbb2ac2a5194bec02f5e28bda7382b937099914
Lipolys <luizfelipedn57@gmail.com>
2024-11-20 03:35:06 -0300

    Data pt-BR deu certo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { PacienteListComponent } from './pages/paciente-list/paciente-list.component';
+import { DatepickerOverviewExampleComponent} from './datepicker-overview-example/datepicker-overview-example.component';
 
 export const routes: Routes = [
   {path:'', component: PacienteListComponent},
+  {path:'datepicker', component: DatepickerOverviewExampleComponent}
 ];
--- src/app/pages/paciente-form/paciente-form.component.ts
+++ src/app/pages/paciente-form/paciente-form.component.ts
@@ -8,31 +8,42 @@ import {
 } from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
 import {MatStep, MatStepLabel, MatStepper, MatStepperPrevious} from '@angular/material/stepper';
-import {MatFormField, MatLabel} from '@angular/material/form-field';
+import {MatFormField, MatFormFieldModule, MatLabel, MatSuffix} from '@angular/material/form-field';
 import {MatInput} from '@angular/material/input';
 import {STEPPER_GLOBAL_OPTIONS} from '@angular/cdk/stepper';
+import {MatDatepicker, MatDatepickerInput, MatDatepickerToggle} from '@angular/material/datepicker';
+import {MAT_DATE_LOCALE, provideNativeDateAdapter} from '@angular/material/core';
 
 @Component({
     selector: 'app-paciente-form',
-    imports: [
-        ReactiveFormsModule,
-        MatDialogContent,
-        MatDialogTitle,
-        MatDialogActions,
-        MatButton,
-        MatDialogClose,
-        MatStep,
-        MatFormField,
-        MatStepper,
-        MatInput,
-        MatStepLabel,
-        MatStepperPrevious,
-        MatLabel
+  imports: [
+    ReactiveFormsModule,
+    MatDialogContent,
+    MatDialogTitle,
+    MatDialogActions,
+    MatButton,
+    MatDialogClose,
+    MatStep,
+    MatFormField,
+    MatFormFieldModule,
+    MatStepper,
+    MatInput,
+    MatStepLabel,
+    MatStepperPrevious,
+    MatLabel,
+    MatDatepicker,
+    MatDatepickerToggle,
+    MatDatepickerInput,
+    MatSuffix
+  ],
+    providers: [
+      {
+        provide: STEPPER_GLOBAL_OPTIONS,
+        useValue: { showError: true }
+      },
+      [provideNativeDateAdapter()],
+      {provide: MAT_DATE_LOCALE, useValue: 'pt-BR'}
     ],
-    providers: [{
-            provide: STEPPER_GLOBAL_OPTIONS,
-            useValue: { showError: true }
-        }],
     templateUrl: './paciente-form.component.html',
     styleUrl: './paciente-form.component.scss'
 })

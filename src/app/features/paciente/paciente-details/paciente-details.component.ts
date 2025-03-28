3b21a0fb8f97876ec7566f728021aeab29207473
Lipolys <luizfelipedn57@gmail.com>
2025-03-28 10:20:39 -0300

    Máscaras e novos campos formulário cliente

--- src/app/features/paciente/paciente-details/paciente-details.component.ts
+++ src/app/features/paciente/paciente-details/paciente-details.component.ts
@@ -2,13 +2,17 @@ import {Component, Inject} from '@angular/core';
 import {MAT_DIALOG_DATA, MatDialogClose, MatDialogRef} from '@angular/material/dialog';
 import {MatButton} from '@angular/material/button';
 import {DatePipe} from '@angular/common';
+import {PhoneFormatPipe} from '../../../sharedpages/phone-format.pipe';
+import {CpfFormatPipe} from '../../../sharedpages/cpf-format.pipe';
 
 @Component({
   selector: 'app-paciente-details',
   imports: [
     MatButton,
     DatePipe,
-    MatDialogClose
+    MatDialogClose,
+    PhoneFormatPipe,
+    CpfFormatPipe
   ],
   templateUrl: './paciente-details.component.html',
   styleUrl: './paciente-details.component.scss'

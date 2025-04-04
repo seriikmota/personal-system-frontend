84356fa24773a03da9e33865caf420b4274e14f9
Lipolys <luizfelipedn57@gmail.com>
2025-04-04 09:00:13 -0300

    Inserindo um botão de logout. TODO implementar

--- src/app/sharedpages/navbar/navbar.component.ts
+++ src/app/sharedpages/navbar/navbar.component.ts
@@ -1,8 +1,10 @@
-import {Component, inject} from '@angular/core';
-import {MatIcon} from '@angular/material/icon';
-import {MatToolbar} from '@angular/material/toolbar';
-import {MatIconButton} from '@angular/material/button';
-import {DrawerService} from '../../services/drawer.service';
+import { Component, inject } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
+import { MatToolbar } from '@angular/material/toolbar';
+import { MatIconButton } from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { DrawerService } from '../../services/drawer.service';
+import { AuthService } from '../../authentication/auth.service';
 
 @Component({
   selector: 'app-navbar',
@@ -10,15 +12,20 @@ import {DrawerService} from '../../services/drawer.service';
   imports: [
     MatIcon,
     MatToolbar,
-    MatIconButton
+    MatIconButton,
+    MatMenuModule
   ],
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent {
 
   private _drawerService = inject(DrawerService);
+  private _authService = inject(AuthService);
 
   toggleDrawer() {
     this._drawerService.toggle();
+  }
+
+  logout() {
   }
 }
